e85f0127894410e30ae164dcdf5179b72032513e
micle-ace <64622804+micle-ace@users.noreply.github.com>
2021-10-12 19:58:32 +0800

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -13,7 +13,8 @@ public class TwoSum {
         }
         return null;
     }
-
+    
+    //Test
     public static void main(String[] args) {
         int[] nums={2,7,11,15};
         int target=17;
